548d2c62f13d1fbd05ce40a0bd37079c6777e76e
Wabene cedric <cedric.wabenemayele.simplon@gmail.com>
2024-05-28 15:14:04 +0200

    initialisation projet

---
+++ backend/src/Entity/Paiement.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaiementRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaiementRepository::class)]
+class Paiement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type_infraction = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_infraction = null;
+
+    #[ORM\Column]
+    private ?int $numéro_carte = null;
+
+    #[ORM\Column]
+    private ?int $numéro_amendes = null;
+
+    #[ORM\Column]
+    private ?int $crypto = null;
+
+    #[ORM\Column]
+    private ?int $date_exp_carte = null;
+
+    #[ORM\Column]
+    private ?int $montant_amende = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $deta_paiement = null;
+
+    #[ORM\ManyToOne(inversedBy: 'paiement')]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTypeInfraction(): ?string
+    {
+        return $this->type_infraction;
+    }
+
+    public function setTypeInfraction(string $type_infraction): static
+    {
+        $this->type_infraction = $type_infraction;
+
+        return $this;
+    }
+
+    public function getDateInfraction(): ?\DateTimeInterface
+    {
+        return $this->date_infraction;
+    }
+
+    public function setDateInfraction(\DateTimeInterface $date_infraction): static
+    {
+        $this->date_infraction = $date_infraction;
+
+        return $this;
+    }
+
+    public function getNuméroCarte(): ?int
+    {
+        return $this->numéro_carte;
+    }
+
+    public function setNuméroCarte(int $numéro_carte): static
+    {
+        $this->numéro_carte = $numéro_carte;
+
+        return $this;
+    }
+
+    public function getNuméroAmendes(): ?int
+    {
+        return $this->numéro_amendes;
+    }
+
+    public function setNuméroAmendes(int $numéro_amendes): static
+    {
+        $this->numéro_amendes = $numéro_amendes;
+
+        return $this;
+    }
+
+    public function getCrypto(): ?int
+    {
+        return $this->crypto;
+    }
+
+    public function setCrypto(int $crypto): static
+    {
+        $this->crypto = $crypto;
+
+        return $this;
+    }
+
+    public function getDateExpCarte(): ?int
+    {
+        return $this->date_exp_carte;
+    }
+
+    public function setDateExpCarte(int $date_exp_carte): static
+    {
+        $this->date_exp_carte = $date_exp_carte;
+
+        return $this;
+    }
+
+    public function getMontantAmende(): ?int
+    {
+        return $this->montant_amende;
+    }
+
+    public function setMontantAmende(int $montant_amende): static
+    {
+        $this->montant_amende = $montant_amende;
+
+        return $this;
+    }
+
+    public function getDetaPaiement(): ?\DateTimeInterface
+    {
+        return $this->deta_paiement;
+    }
+
+    public function setDetaPaiement(\DateTimeInterface $deta_paiement): static
+    {
+        $this->deta_paiement = $deta_paiement;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
---
+++ backend/src/Entity/User.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $prénom = null;
+
+    #[ORM\Column(length: 200)]
+    private ?string $adresse = null;
+
+    #[ORM\Column]
+    private ?int $numero_de_téléphone = null;
+
+    /**
+     * @var Collection<int, paiement>
+     */
+    #[ORM\OneToMany(targetEntity: paiement::class, mappedBy: 'user')]
+    private Collection $paiement;
+
+    public function __construct()
+    {
+        $this->paiement = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrénom(): ?string
+    {
+        return $this->prénom;
+    }
+
+    public function setPrénom(string $prénom): static
+    {
+        $this->prénom = $prénom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getNumeroDeTéléphone(): ?int
+    {
+        return $this->numero_de_téléphone;
+    }
+
+    public function setNumeroDeTéléphone(int $numero_de_téléphone): static
+    {
+        $this->numero_de_téléphone = $numero_de_téléphone;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, paiement>
+     */
+    public function getPaiement(): Collection
+    {
+        return $this->paiement;
+    }
+
+    public function addPaiement(paiement $paiement): static
+    {
+        if (!$this->paiement->contains($paiement)) {
+            $this->paiement->add($paiement);
+            $paiement->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removePaiement(paiement $paiement): static
+    {
+        if ($this->paiement->removeElement($paiement)) {
+            // set the owning side to null (unless already changed)
+            if ($paiement->getUser() === $this) {
+                $paiement->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+}
